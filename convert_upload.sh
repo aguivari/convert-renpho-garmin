e1e348f4f7a48b2594ff6999a732ad8665cb057f
Adilson Ribeiro <aguivari.ie@gmail.com>
2026-05-13 15:55:58 +0100

    change parameter order for convert&upload script

--- convert_upload.sh
+++ convert_upload.sh
@@ -2,16 +2,16 @@
 
 if [ -z "$1" ];
 then
-  echo "needed arguments: <CSV file to process> <scale brand> <gender>"
+  echo "needed arguments: <scale brand> <gender> <CSV file to process>"
   exit 1
 fi
 
 filename="$(date +%Y%m%d%H%M%S).fit"
-directory=$(dirname "$1")
+directory=$(dirname "$3")
 
 real_path=$(realpath "${BASH_SOURCE[0]}")
 script_dir=$(dirname "$real_path")
 
-python "${script_dir}/convert-scale-garmin.py" "$2" "$1" "$directory/$filename"  "$3"
+python "${script_dir}/convert-scale-garmin.py" "$1" "$3" "$directory/$filename" "$2"
 python "${script_dir}/uploader.py" "$directory/$filename"
 
